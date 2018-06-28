bf52199ea5b5d5f5c53f93c73d23d3bfd92184a9
DAGNOGO Jean-François <jdagnogo11@gmail.com>
2018-06-28 14:32:34 +0200

    first test with streetView and kotlin

---
+++ app/src/main/java/com/example/jdagnogo/streetviewwithkotlin/PanoramaActivity.kt
@@ -0,0 +1,28 @@
+package com.example.jdagnogo.streetviewwithkotlin
+
+import android.os.Bundle
+import android.support.v4.app.FragmentActivity
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback
+import com.google.android.gms.maps.StreetViewPanorama
+import com.google.android.gms.maps.StreetViewPanoramaFragment
+import com.google.android.gms.maps.model.LatLng
+
+
+class PanoramaActivity : FragmentActivity(), OnStreetViewPanoramaReadyCallback {
+
+    override fun onStreetViewPanoramaReady(panorama: StreetViewPanorama?) {
+        if (panorama != null) {
+            panorama.setPosition(LatLng(44.827646, -0.575700))
+
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_panorama)
+
+        val streetViewPanoramaFragment = fragmentManager
+                .findFragmentById(R.id.streetviewpanorama) as StreetViewPanoramaFragment
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this)
+    }
+}
